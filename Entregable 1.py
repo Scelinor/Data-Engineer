4db7a0b71db2aaf97520feb4094913afbc841b0c
Juana <juanaescobarpinzon@hotmail.com>
2023-08-29 19:58:44 -0300

    Tercera entrega

--- Entregable 1.py
+++ Entregable 1.py
@@ -5,6 +5,22 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
 
+#Primero hacemos el diccionario con las ciudades
+cities = [
+    {'station': '87453', 'name': 'Rio Cuarto'},
+    {'station': '87576', 'name': 'Ezeiza'},
+    {'station': '87345', 'name': 'Cordoba'},
+    {'station': '87571', 'name': 'Moron'},
+    {'station': '87480', 'name': 'Rosario'},
+    {'station': '87497', 'name': 'Gualeguaychu'},
+    {'station': '87047', 'name': 'Salta'},
+    {'station': '87046', 'name': 'San Salvador de Jujuy'},
+    {'station': '87121', 'name': 'San Miguel de Tucuman'},
+    {'station': '87217', 'name': 'La Rioja'}
+]
+# Lista para almacenar los datos de todas las ciudades
+all_filtered_data = []
+
 #Declaracion de credenciales + conexion
 credenciales = {
     'username': 'juanaescobarpinzon_coderhouse',
@@ -17,38 +33,46 @@ db_conn_str = f"postgresql://{credenciales['username']}:{credenciales['password'
 conn = create_engine(db_conn_str)
 
 #Request de la API meteostat
-url = "https://meteostat.p.rapidapi.com/stations/daily"
+for city_info in cities:
+    city = city_info['station']
+    name = city_info['name']
 
-querystring = {"station":"10637","start":"2023-08-01","end":"2023-12-01"}
-headers = {
-    "X-RapidAPI-Key": "a5b08030f7mshb2c9409a627ea15p10ce88jsn90cea41c9648",
-    "X-RapidAPI-Host": "meteostat.p.rapidapi.com"
-}
+    url = "https://meteostat.p.rapidapi.com/stations/daily"
 
-response = requests.get(url, headers=headers, params=querystring)
-
-data = response.json()
-
-#Aca se filtran los campos que quiero tener y los puedo renombrar
-filtered_data = []
-for entry in data['data']:
-    filtered_entry = {
-        'fecha': entry['date'],
-        'temp_promedio': entry['tavg'],
-        'temp_minima': entry['tmin'],
-        'temp_maxima': entry['tmax'],
-        'precipitaciones': entry['prcp'],
-        'nieve': entry['snow'],
-        'dir_viento': entry['wdir'],
-        'vel_viento': entry['wspd'],
-        'presion': entry['pres'],
-        'minutos_atardecer': entry['tsun']
-        
+    querystring = {"station":city,"start":"2023-08-01","end":"2023-12-01"}
+    headers = {
+        "X-RapidAPI-Key": "a5b08030f7mshb2c9409a627ea15p10ce88jsn90cea41c9648",
+        "X-RapidAPI-Host": "meteostat.p.rapidapi.com"
     }
-    filtered_data.append(filtered_entry)
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    data = response.json()
+
+    #Aca se filtran los campos que quiero tener 
+    filtered_data = []
+    for entry in data['data']:
+        filtered_entry = {
+            'fecha': entry['date'],
+            'ciudad':name, #agregado ultimo
+            'temp_promedio': entry['tavg'],
+            'temp_minima': entry['tmin'],
+            'temp_maxima': entry['tmax'],
+            'precipitaciones': entry['prcp'],
+            'nieve': entry['snow'],
+            'dir_viento': entry['wdir'],
+            'vel_viento': entry['wspd'],
+            'presion': entry['pres'],
+            'minutos_atardecer': entry['tsun']
+            
+        }
+        filtered_data.append(filtered_entry)
+
+    all_filtered_data.extend(filtered_data)
 
 #Crear el dataframe
-df = pd.DataFrame(filtered_data)
+df = pd.DataFrame(all_filtered_data)
+
 
 table_name = 'prueba_clima'
 
